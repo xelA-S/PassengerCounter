4453e33a60da089ad416f6d64f75a5d870794df9
xelA-S <asmith6398@gmail.com>
2021-08-13 04:06:14 +0100

    added increment and save button

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask,render_template,url_for
 
 app=Flask(__name__)
 
-@app.route
+@app.route("/",methods=["GET"])
 def index():
     return render_template("index.html")
 
---
+++ static/js/index.js
@@ -0,0 +1,16 @@
+let count = 0
+
+let counter = document.getElementById("counter")
+let save = document.getElementById("save")
+
+
+
+function increment(){
+    count+=1
+    counter.innerText=count
+}
+
+function saving(){
+    save.innerText+=" " +count + " -"
+    //save.textContent += count + " - " can also be used as it does not remove previously added spaces
+}
